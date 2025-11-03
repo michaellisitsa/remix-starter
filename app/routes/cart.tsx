a64e4a4d5d94440bd119e20726a6c5174eceebd9
Michael Lisitsa <michael@lisitsa.dev>
2025-11-03 15:46:48 +1100

    Add array of objects to form field

--- app/routes/cart.tsx
+++ app/routes/cart.tsx
@@ -1,3 +1,4 @@
+import { parseWithZod } from "@conform-to/zod/v4";
 import type { Route } from "./+types/home";
 import { Form, redirect } from "react-router";
 
@@ -6,16 +7,20 @@ import { z } from "zod";
 const schema = z.object({
   // The preprocess step is required for zod to perform the required check properly
   // as the value of an empty input is usually an empty string
-  email: z.preprocess(
-    (value) => (value === "" ? undefined : value),
-    z.string({ required_error: "Email is required" }).email("Email is invalid"),
-  ),
-  message: z.preprocess(
-    (value) => (value === "" ? undefined : value),
-    z
-      .string({ required_error: "Message is required" })
-      .min(2, "Message is too short")
-      .max(100, "Message is too long"),
+  email: z
+    .string({ required_error: "Email is required" })
+    .email("Email is invalid"),
+  message: z
+    .string({ required_error: "Message is required" })
+    .min(2, "Message is too short")
+    .max(100, "Message is too long"),
+  products: z.array(
+    z.object({
+      id: z.string(),
+      title: z.string(),
+      userId: z.string(),
+      completed: z.string(),
+    }),
   ),
 });
 
@@ -29,36 +34,31 @@ export function meta({}: Route.MetaArgs) {
 export async function clientAction({ request }: Route.ClientActionArgs) {
   let formData = await request.formData();
   // Construct an object using `Object.fromEntries`
-  const payload = Object.fromEntries(formData);
-  // Then parse it with zod
-  const result = schema.safeParse(payload);
-  // Return the error to the client if the data is not valid
-  if (!result.success) {
-    const error = result.error.flatten();
-    return {
-      payload,
-      formErrors: error.formErrors,
-      fieldErrors: error.fieldErrors,
-    };
+  const submission = parseWithZod(formData, { schema });
+
+  // First return conform validation
+  if (submission.status !== "success") {
+    return submission.reply();
   }
 
   const message = await fetch("https://jsonplaceholder.typicode.com/posts", {
     method: "POST",
-    body: JSON.stringify(result.data),
+    body: JSON.stringify(submission.value),
   });
+
   const response = await message.json();
   console.log("Log → cart.tsx 45: message", response);
 
   if (!message.ok) {
     return {
-      payload,
+      payload: submission.value,
       formErrors: ["Failed to send the message. Please try again later."],
       fieldErrors: {},
     };
   }
 
   return {
-    payload,
+    payload: submission.value,
     ...response,
   };
 }
@@ -77,10 +77,6 @@ export function HydrateFallback() {
 }
 export default function Cart({ loaderData, actionData }: Route.ComponentProps) {
   const data = loaderData;
-  data?.forEach((item) => {
-    const { id, title, userId, completed } = item;
-    console.log("Log → cart.tsx 39: title", title);
-  });
 
   return (
     <main>
@@ -91,16 +87,38 @@ export default function Cart({ loaderData, actionData }: Route.ComponentProps) {
           <input
             type="email"
             name="email"
-            defaultValue={actionData?.payload.email}
+            defaultValue={actionData?.payload?.email}
           />
           <div>{actionData?.fieldErrors?.email}</div>
         </div>
         <div>
           <label>Message</label>
-          <textarea name="message" defaultValue={actionData?.payload.message} />
+          <textarea
+            name="message"
+            defaultValue={actionData?.payload?.message}
+          />
           <div>{actionData?.fieldErrors?.message}</div>
         </div>
-        <button>Send</button>
+        <br />
+        <ul>
+          {data?.map((item, idx) => {
+            const { id, title, userId, completed } = item;
+            console.log("Log → cart.tsx 39: title", title);
+            return (
+              <li>
+                <label>title {title}</label>
+                <input name={`products[${idx}].title`} value={title} />
+                <label>id {id}</label>
+                <input name={`products[${idx}].id`} value={id} />
+                <label>userId {userId}</label>
+                <input name={`products[${idx}].userId`} value={userId} />
+                <label>completed {completed}</label>
+                <input name={`products[${idx}].completed`} value={completed} />
+              </li>
+            );
+          })}
+        </ul>
+        <button className="border-solid">Send</button>
         <br />
         <b>Created id: {actionData?.id}</b>
       </Form>
